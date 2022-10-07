9773a442f5147e9a21fea7866cf8e84d6b88dd0b
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-07 23:20:49 +0200

    cart  app using javascript completed

---
+++ script.js
@@ -0,0 +1,21 @@
+let productDetails = [];
+
+let pdtName = document.querySelector(".pdtName");
+let price = document.querySelector(".price");
+let itemAdded = document.querySelector(".itemAdded");
+let priceCal = document.querySelector(".priceCal");
+
+
+function addToCart() {
+    //product item
+    let itemName = document.createElement('p');
+    itemName.innerHTML = pdtName.innerHTML;
+    itemAdded.append(itemName);
+
+
+    //product price
+    let priceAdd = document.createElement('p');
+    priceAdd.innerHTML = priceCal.innerHTML;
+    itemAdded.append(priceAdd);
+
+}
