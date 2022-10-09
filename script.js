3b5052996ddeaad89f611a377b4116306f5a1e03
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-09 17:51:34 +0200

    cart app partially completed

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let pdtName = document.querySelector(".pdtName");
 // let price = document.querySelector(".price");
 
 //let itemAdded = document.querySelector(".itemAdded");
-//let priceCal = document.querySelector(".priceCal");
+let priceCal = document.querySelector(".priceCal");
+//console.log(priceCal.innerHTML);
+
+
 let itemDivContainer = document.querySelector(".itemDivContainer");
 
 let buttonCart = document.querySelectorAll('.buttonCart');
@@ -17,7 +20,17 @@ function addToCart(event) {
     let division = event.target.parentElement.parentElement.parentElement;
     let pdtName = division.children[1].children[0];
     let price = division.children[4].children[1];
-    console.log(price);
+
+    let subtotal = priceCal.innerHTML;
+    let itemPrice;
+
+    //remove $ from the value
+    subtotal = subtotal.substring(1);
+
+    //Replace comma with Empty character
+    subtotal = subtotal.replace(/,/g, "");
+
+    subtotal = parseInt(subtotal);
 
     let itemDiv = document.querySelector('.itemDiv');
     let cloneItemDiv = itemDiv.cloneNode(true);
@@ -27,5 +40,20 @@ function addToCart(event) {
     cloneItemDiv.children[0].innerText = pdtName.innerHTML;
     cloneItemDiv.children[1].innerText = price.innerHTML;
 
+    itemPrice = price.innerHTML.substring(1);
+
+    itemPrice = itemPrice.replace(/,/g, "");
+
+
+
+    itemPrice = parseInt(itemPrice);
+
+
+    subtotal = subtotal + itemPrice;
+
+    // Add $ and comma into the number
+
+
+    priceCal.innerHTML = "$" + subtotal.toLocaleString();
 
 }
